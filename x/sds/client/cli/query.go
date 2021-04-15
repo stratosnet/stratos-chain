1e2b81c72004a2a1761c5c6b62c0435ae4fe5635
Marc-Antoine Dumais <marc-antoine@qsnetwork.org>
2021-04-15 08:42:45 -0400

    git hook did its job

--- x/sds/client/cli/query.go
+++ x/sds/client/cli/query.go
@@ -29,7 +29,7 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 
 	stratoschainQueryCmd.AddCommand(
 		flags.GetCommands(
-      // this line is used by starport scaffolding # 1
+		// this line is used by starport scaffolding # 1
 		)...,
 	)
 
--- x/sds/handler.go
+++ x/sds/handler.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stratosnet/stratos-chain/x/sds/keeper"
 	"github.com/stratosnet/stratos-chain/x/sds/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // NewHandler ...
@@ -14,7 +14,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
-    // this line is used by starport scaffolding # 1
+		// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
