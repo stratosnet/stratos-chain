653b729d524fe5fb82a2c6c8f8526ec203c96d1a
xiong-qsnetwork <xixiongchen@qsnetwork.org>
2021-05-06 17:10:55 -0400

    add log

--- x/register/types/genesis.go
+++ x/register/types/genesis.go
@@ -15,14 +15,14 @@ type GenesisState struct {
 
 // LastResourceNodePower required for resource node set update logic
 type LastResourceNodePower struct {
-	Address sdk.AccAddress
-	Power   int64
+	Address sdk.AccAddress `json:"address" yaml:"address"`
+	Power   int64          `json:"power" yaml:"power"`
 }
 
 // LastIndexingNodePower required for indexing node set update logic
 type LastIndexingNodePower struct {
-	Address sdk.AccAddress
-	Power   int64
+	Address sdk.AccAddress `json:"address" yaml:"address"`
+	Power   int64          `json:"power" yaml:"power"`
 }
 
 // NewGenesisState creates a new GenesisState object
