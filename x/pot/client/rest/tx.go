ac1005dbf7bd4a92f8bff765c2e6e0a1a1730a55
hong-pang <hongpang@qsnetwork.org>
2021-09-08 20:54:39 -0400

    Added and tested POT withdraw, volume report tx APIS

--- x/pot/client/rest/tx.go
+++ x/pot/client/rest/tx.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
@@ -13,18 +14,27 @@ import (
 //RegisterRoutes registers pot-related REST handlers to a router
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/pot/volume/report", VolumeReportRequestHandlerFn(cliCtx)).Methods("POST")
+	r.HandleFunc("/pot/address/{nodeAddr}/rewards", withdrawPotRewardsHandlerFn(cliCtx)).Methods("POST")
 }
 
 // VolumeReportReq defines the properties of a send request's body.
-type VolumeReportReq struct {
-	BaseReq         rest.BaseReq             `json:"base_req" yaml:"base_req"`
-	NodesVolume     []types.SingleNodeVolume `json:"nodes_volume" yaml:"nodes_volume"`         // volume report
-	Reporter        string                   `json:"reporter" yaml:"reporter"`                 // volume reporter
-	Epoch           int64                    `json:"report_epoch" yaml:"report_epoch"`         // volume report epoch
-	ReportReference string                   `json:"report_reference" yaml:"report_reference"` // volume report reference
-}
+type (
+	withdrawRewardsReq struct {
+		BaseReq    rest.BaseReq `json:"base_req" yaml:"base_req"`
+		Amount     string       `json:"amount" yaml:"amount"`
+		TargetAddr string       `json:"target_addr" yaml:"target_addr"`
+	}
 
-// VolumeReportRequestHandlerFn - http request handler to send coins to a address.
+	VolumeReportReq struct {
+		BaseReq         rest.BaseReq             `json:"base_req" yaml:"base_req"`
+		NodesVolume     []types.SingleNodeVolume `json:"nodes_volume" yaml:"nodes_volume"`         // volume report
+		Reporter        string                   `json:"reporter" yaml:"reporter"`                 // volume reporter
+		Epoch           int64                    `json:"report_epoch" yaml:"report_epoch"`         // volume report epoch
+		ReportReference string                   `json:"report_reference" yaml:"report_reference"` // volume report reference
+	}
+)
+
+// VolumeReportRequestHandlerFn rest API handler to create a volume report tx.
 func VolumeReportRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req VolumeReportReq
@@ -68,4 +78,75 @@ func VolumeReportRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 		}
 		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
 	}
+}
+
+// rest API handler Withdraw pot rewards
+func withdrawPotRewardsHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req withdrawRewardsReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		// read and validate URL's variables
+		amountStr := req.Amount
+		amount, ok := checkAmountVar(w, r, amountStr)
+		if !ok {
+			return
+		}
+		nodeAddrStr := mux.Vars(r)["nodeAddr"]
+		nodeAddr, ok := checkAccountAddressVar(w, r, nodeAddrStr)
+		if !ok {
+			return
+		}
+
+		targetAddrStr := req.TargetAddr
+		targetAddr, ok := checkAccountAddressVar(w, r, targetAddrStr)
+		if !ok {
+			return
+		}
+
+		//TODO: Add targetAddr after NewMsgWithdraw updates
+		fmt.Println("targetAddr", targetAddr)
+
+		ownerAddrStr := req.BaseReq.From
+		ownerAddr, ok := checkAccountAddressVar(w, r, ownerAddrStr)
+		if !ok {
+			return
+		}
+
+		msg := types.NewMsgWithdraw(sdk.NewCoin(types.DefaultBondDenom, amount), nodeAddr, ownerAddr)
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}
+
+func checkAccountAddressVar(w http.ResponseWriter, r *http.Request, accountAddrStr string) (sdk.AccAddress, bool) {
+	addr, err := sdk.AccAddressFromBech32(accountAddrStr)
+	if err != nil {
+		rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+		return nil, false
+	}
+
+	return addr, true
+}
+
+func checkAmountVar(w http.ResponseWriter, r *http.Request, amountStr string) (sdk.Int, bool) {
+	amount, ok := sdk.NewIntFromString(amountStr)
+	if !ok {
+		rest.WriteErrorResponse(w, http.StatusBadRequest, "invalid withdraw amount")
+		return sdk.NewInt(0), false
+	}
+	return amount, true
 }
