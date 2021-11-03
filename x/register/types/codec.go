1fd70707481e63f94995b09e356c80ec05f3cad0
jialbai <maodou_bjl@126.com>
2021-11-03 10:10:34 -0400

    - qb 615: Change tx name in codec

--- x/register/types/codec.go
+++ x/register/types/codec.go
@@ -9,12 +9,12 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgCreateResourceNode{}, "register/MsgCreateResourceNode", nil)
 	cdc.RegisterConcrete(MsgRemoveResourceNode{}, "register/MsgRemoveResourceNode", nil)
 	cdc.RegisterConcrete(MsgUpdateResourceNode{}, "register/MsgUpdateResourceNode", nil)
-	cdc.RegisterConcrete(MsgUpdateResourceNodeStake{}, "register/UpdateResourceNodeTx", nil)
+	cdc.RegisterConcrete(MsgUpdateResourceNodeStake{}, "register/UpdateResourceNodeStakeTx", nil)
 
 	cdc.RegisterConcrete(MsgCreateIndexingNode{}, "register/MsgCreateIndexingNode", nil)
 	cdc.RegisterConcrete(MsgRemoveIndexingNode{}, "register/MsgRemoveIndexingNode", nil)
 	cdc.RegisterConcrete(MsgUpdateIndexingNode{}, "register/MsgUpdateIndexingNode", nil)
-	cdc.RegisterConcrete(MsgUpdateIndexingNodeStake{}, "register/UpdateIndexingNodeTx", nil)
+	cdc.RegisterConcrete(MsgUpdateIndexingNodeStake{}, "register/UpdateIndexingNodeStakeTx", nil)
 
 	cdc.RegisterConcrete(MsgIndexingNodeRegistrationVote{}, "register/MsgIndexingNodeRegistrationVote", nil)
 }
