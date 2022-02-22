48b026a52f29066a975bf37851ff603277b7b8bd
jialbai <maodou_bjl@126.com>
2022-02-22 11:45:36 -0500

    - qb1010: fix panic for pot/withdraw

--- x/pot/keeper/withdraw.go
+++ x/pot/keeper/withdraw.go
@@ -1,11 +1,16 @@
 package keeper
 
 import (
+	"errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k Keeper) Withdraw(ctx sdk.Context, amount sdk.Coins, walletAddress sdk.AccAddress, targetAddress sdk.AccAddress) error {
 	matureReward := k.GetMatureTotalReward(ctx, walletAddress)
+	if !matureReward.IsAllGTE(amount) {
+		return errors.New("insufficient reward to be withdrawn")
+	}
 	matureRewardBalance := matureReward.Sub(amount)
 	_, err := k.BankKeeper.AddCoins(ctx, targetAddress, amount)
 	if err != nil {
