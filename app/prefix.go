27ffa36f7375cf0574b927d03ec464a36bf9538a
alexstratos <alex@thestratos.org>
2022-03-09 15:41:54 -0500

    fix the config of prefix and hd path

--- app/prefix.go
+++ app/prefix.go
@@ -4,22 +4,8 @@ import (
 	stratos "github.com/stratosnet/stratos-chain/types"
 )
 
-const (
-	StratosBech32Prefix = "st"
-
-	AccountPubKeyPrefix    = StratosBech32Prefix + "pub"
-	ValidatorAddressPrefix = StratosBech32Prefix + "valoper"
-	ValidatorPubKeyPrefix  = StratosBech32Prefix + "valoperpub"
-	ConsNodeAddressPrefix  = StratosBech32Prefix + "valcons"
-	ConsNodePubKeyPrefix   = StratosBech32Prefix + "valconspub"
-	SdsNodeP2PKeyPrefix    = StratosBech32Prefix + "sdsp2p"
-)
-
 func SetConfig() {
 	config := stratos.GetConfig()
-	config.SetBech32PrefixForAccount(StratosBech32Prefix, AccountPubKeyPrefix)
-	config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
-	config.SetBech32PrefixForConsensusNode(ConsNodeAddressPrefix, ConsNodePubKeyPrefix)
-	config.SetBech32PrefixForSdsNodeP2P(SdsNodeP2PKeyPrefix)
+
 	config.Seal()
 }
