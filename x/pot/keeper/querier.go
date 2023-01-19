bf66b06fe4419dd6d7484e428e5fa8e1c151b0e1
Xiong <xixiongchen@thestratos.org>
2023-01-19 16:20:13 -0500

    Optimize queries

--- x/pot/keeper/querier.go
+++ x/pot/keeper/querier.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/cosmos/cosmos-sdk/client"
+	abci "github.com/tendermint/tendermint/abci/types"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/stratosnet/stratos-chain/client"
 	"github.com/stratosnet/stratos-chain/x/pot/types"
-	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 const (
-	QueryVolumeReport            = "query_volume_report"
-	QueryPotRewardsByReportEpoch = "query_pot_rewards_by_report_epoch"
-	QueryPotRewardsByWalletAddr  = "query_pot_rewards_by_wallet_address"
-	QueryPotSlashingByWalletAddr = "query_pot_slashing_by_wallet_address"
-	QueryPotParams               = "query_pot_params"
-	QueryDefaultLimit            = 100
+	QueryVolumeReport                   = "query_volume_report"
+	QueryIndividualRewardsByReportEpoch = "query_pot_individual_rewards_by_report_epoch"
+	QueryRewardsByWalletAddr            = "query_pot_rewards_by_wallet_address"
+	QuerySlashingByWalletAddr           = "query_pot_slashing_by_wallet_address"
+	QueryPotParams                      = "query_pot_params"
+	QueryDefaultLimit                   = 100
 )
 
 // NewQuerier creates a new querier for pot clients.
@@ -27,12 +29,12 @@ func NewQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
 		switch path[0] {
 		case QueryVolumeReport:
 			return queryVolumeReport(ctx, req, k, legacyQuerierCdc)
-		case QueryPotRewardsByReportEpoch:
-			return queryPotRewardsByReportEpoch(ctx, req, k, legacyQuerierCdc)
-		case QueryPotRewardsByWalletAddr:
-			return queryPotRewardsByWalletAddress(ctx, req, k, legacyQuerierCdc)
-		case QueryPotSlashingByWalletAddr:
-			return queryPotSlashingByWalletAddress(ctx, req, k, legacyQuerierCdc)
+		case QueryIndividualRewardsByReportEpoch:
+			return queryIndividualRewardsByReportEpoch(ctx, req, k, legacyQuerierCdc)
+		case QueryRewardsByWalletAddr:
+			return queryRewardsByWalletAddress(ctx, req, k, legacyQuerierCdc)
+		case QuerySlashingByWalletAddr:
+			return querySlashingByWalletAddress(ctx, req, k, legacyQuerierCdc)
 		case QueryPotParams:
 			return getPotParams(ctx, req, k, legacyQuerierCdc)
 		default:
@@ -72,14 +74,14 @@ func queryVolumeReport(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQ
 	return bz, nil
 }
 
-// queryPotRewardsByReportEpoch fetches total rewards and owner individual rewards from traffic and mining.
-func queryPotRewardsByReportEpoch(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
-	var params types.QueryPotRewardsByReportEpochParams
+// queryPotRewardsByReportEpoch fetches individual rewards from traffic and mining pool.
+func queryIndividualRewardsByReportEpoch(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+	var params types.QueryIndividualRewardsByReportEpochParams
 	err := legacyQuerierCdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
 		return []byte{}, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
-	potEpochRewards := k.getPotRewardsByReportEpoch(ctx, params)
+	potEpochRewards := k.getIndividualRewardsByReportEpoch(ctx, params)
 	if len(potEpochRewards) < 1 {
 		e := sdkerrors.Wrapf(types.ErrCannotFindReward, fmt.Sprintf("no Pot rewards information at epoch %s", params.Epoch.String()))
 		return []byte{}, e
@@ -91,56 +93,50 @@ func queryPotRewardsByReportEpoch(ctx sdk.Context, req abci.RequestQuery, k Keep
 	return bz, nil
 }
 
-func (k Keeper) getPotRewardsByReportEpoch(ctx sdk.Context, params types.QueryPotRewardsByReportEpochParams) (res []types.Reward) {
+func (k Keeper) getIndividualRewardsByReportEpoch(ctx sdk.Context, params types.QueryIndividualRewardsByReportEpochParams) (res []types.Reward) {
 	matureEpoch := params.Epoch.Add(sdk.NewInt(k.MatureEpoch(ctx)))
 
+	start, end := client.Paginate(params.Page, params.Limit, QueryDefaultLimit)
+	if start < 0 || end < 0 {
+		return nil
+	}
+
+	index := 0
 	k.IteratorIndividualReward(ctx, matureEpoch, func(walletAddress sdk.AccAddress, individualReward types.Reward) (stop bool) {
 		if !((individualReward.RewardFromMiningPool.Empty() || individualReward.RewardFromMiningPool.IsZero()) &&
 			(individualReward.RewardFromTrafficPool.Empty() || individualReward.RewardFromTrafficPool.IsZero())) {
-			res = append(res, individualReward)
+			if index >= end {
+				return true
+			}
+			if index >= start && index < end {
+				res = append(res, individualReward)
+			}
+			index++
 		}
 		return false
 	})
 
-	start, end := client.Paginate(len(res), params.Page, params.Limit, QueryDefaultLimit)
-	if start < 0 || end < 0 {
-		return nil
-	} else {
-		res = res[start:end]
-		return res
-	}
+	return res
 }
 
-func (k Keeper) getPotRewardsByWalletAddressAndEpoch(ctx sdk.Context, params types.QueryPotRewardsByWalletAddrParams) (res []types.Reward) {
-	matureEpoch := params.Epoch.Add(sdk.NewInt(k.MatureEpoch(ctx)))
-
-	reward, found := k.GetIndividualReward(ctx, params.WalletAddr, matureEpoch)
-	if found {
-		res = append(res, reward)
-	}
-
-	start, end := client.Paginate(len(res), params.Page, params.Limit, QueryDefaultLimit)
-	if start < 0 || end < 0 {
-		return nil
-	} else {
-		res = res[start:end]
-		return res
-	}
-}
-
-func queryPotRewardsByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
-	var params types.QueryPotRewardsByWalletAddrParams
+// When param "Epoch" not exists: Returns wallet_address, mature_total & immature_total
+// When Param "Epoch" exists: Returns walletAddress & individual_reward at that epoch
+func queryRewardsByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+	var params types.QueryRewardsByWalletAddrParams
 	err := legacyQuerierCdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
 	}
 
+	// param epoch exists
 	if !params.Epoch.Equal(sdk.ZeroInt()) {
-		rewardsByByWalletAddressAndEpoch := k.getPotRewardsByWalletAddressAndEpoch(ctx, params)
-		if len(rewardsByByWalletAddressAndEpoch) < 1 {
+		matureEpoch := params.Epoch.Add(sdk.NewInt(k.MatureEpoch(ctx)))
+		rewardsByByWalletAddressAndEpoch, found := k.GetIndividualReward(ctx, params.WalletAddr, matureEpoch)
+		if !found {
 			e := sdkerrors.Wrapf(types.ErrCannotFindReward, fmt.Sprintf("no Pot rewards information at epoch %s", params.Epoch.String()))
 			return []byte{}, e
 		}
+
 		bz, err := codec.MarshalJSONIndent(legacyQuerierCdc, rewardsByByWalletAddressAndEpoch)
 		if err != nil {
 			return []byte{}, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
@@ -148,6 +144,7 @@ func queryPotRewardsByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Ke
 		return bz, nil
 	}
 
+	// param epoch not exists
 	immatureTotalReward := k.GetImmatureTotalReward(ctx, params.WalletAddr)
 	matureTotalReward := k.GetMatureTotalReward(ctx, params.WalletAddr)
 	reward := types.NewPotRewardInfo(params.WalletAddr, matureTotalReward, immatureTotalReward)
@@ -159,7 +156,7 @@ func queryPotRewardsByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Ke
 	return bz, nil
 }
 
-func queryPotSlashingByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Keeper, _ *codec.LegacyAmino) ([]byte, error) {
+func querySlashingByWalletAddress(ctx sdk.Context, req abci.RequestQuery, k Keeper, _ *codec.LegacyAmino) ([]byte, error) {
 	addr, err := sdk.AccAddressFromBech32(string(req.Data))
 	if err != nil {
 		return []byte(sdk.ZeroInt().String()), types.ErrUnknownAccountAddress
