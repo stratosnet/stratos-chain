de18eda1863a543668cc5e2e5ff48a1073c98fde
BoThe1K <bohdan@thestratos.org>
2023-02-11 01:26:33 +0100

    Add EVM state breakers on new prod

--- app/ante/handler_options.go
+++ app/ante/handler_options.go
@@ -48,8 +48,7 @@ func newEthAnteHandler(options HandlerOptions) sdk.AnteHandler {
 		NewEthSetUpContextDecorator(options.EvmKeeper), // outermost AnteDecorator. SetUpContext must be called first
 		NewEthMempoolFeeDecorator(options.EvmKeeper),   // Check eth effective gas price against minimal-gas-prices
 		NewEthValidateBasicDecorator(options.EvmKeeper),
-		// TODO: UNCOMMENT THIS FOR PROD!!!!!!
-		// NewEthTxPayloadVerificationDecorator(),
+		NewEthTxPayloadVerificationDecorator(),
 		NewEthSigVerificationDecorator(options.EvmKeeper),
 		NewEthAccountVerificationDecorator(options.AccountKeeper, options.EvmKeeper),
 		NewEthGasConsumeDecorator(options.EvmKeeper, options.MaxTxGasWanted),
