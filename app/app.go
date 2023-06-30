00c627483aa04d2e20d5b84d7d42ed36f2c57bc5
Xiong <xixiongchen@thestratos.org>
2023-06-30 10:34:28 -0400

    register upgrade handler

--- app/app.go
+++ app/app.go
@@ -578,6 +578,10 @@ func NewInitApp(
 	app.configurator = module.NewConfigurator(app.appCodec, app.MsgServiceRouter(), app.GRPCQueryRouter())
 	app.mm.RegisterServices(app.configurator)
 
+	// RegisterUpgradeHandlers is used for registering any on-chain upgrades.
+	// Make sure it's called after `app.mm` and `app.configurator` are set.
+	app.RegisterUpgradeHandlers()
+
 	// create the simulation manager and define the order of the modules for deterministic simulations
 
 	// NOTE: this is not required apps that don't use the simulator for fuzz testing
