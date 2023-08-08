fa46e7f94d441c869b0fa1da06e3d37e4f5372aa
Xiong <xixiongchen@thestratos.org>
2023-08-08 17:05:35 -0400

    update

--- x/register/abci.go
+++ x/register/abci.go
@@ -11,7 +11,7 @@ import (
 // BeginBlocker check for infraction evidence or downtime of validators
 // on every begin block
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
-	k.UpdateMetaNodeBitMapIdxCache(ctx)
+	//k.UpdateMetaNodeBitMapIdxCache(ctx)
 }
 
 // EndBlocker called every block, process inflation, update validator set.
