8945491e4ad154ae5b8e3d018aa29210e296ed86
Xiong <xixiongchen@thestratos.org>
2024-04-09 12:41:08 -0400

    add ethermint ethsecp256k1 key

--- crypto/codec/proto.go
+++ crypto/codec/proto.go
@@ -5,10 +5,13 @@ import (
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 
 	"github.com/stratosnet/stratos-chain/crypto/ethsecp256k1"
+	ethermintethsecp256k1 "github.com/stratosnet/stratos-chain/crypto/ethsecp256k1"
 )
 
 // RegisterInterfaces register the stratos key concrete types.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethsecp256k1.PubKey{})
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
+	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethermintethsecp256k1.PubKey{})
+	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethermintethsecp256k1.PrivKey{})
 }
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -8,7 +8,7 @@ set -e
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./stratos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
